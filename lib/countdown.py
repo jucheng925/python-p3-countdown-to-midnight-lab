82e132e4ba28731c638a021c0de0f4fb43f4d4a5
Julie C <jucheng925@gmail.com>
2023-09-20 14:16:56 -0400

    countdown with timer

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
-# your code goes here!
+import time
+
+def countdown(sec):
+    while sec > 0:
+        print(f'{sec} SECOND(S)!')
+        sec -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(sec):
+    while sec > 0:
+        print(f'{sec} SECOND(S)!')
+        sec -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+
+
+countdown_with_sleep(5)
